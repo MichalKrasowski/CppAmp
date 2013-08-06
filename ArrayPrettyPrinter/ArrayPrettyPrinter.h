e0871057ef7fe6bbc951df878baddd7bb5601ac7
videl.prv@gmail.com <videl.prv@gmail.com>
2013-08-06 18:36:27 +0200

    Added a comment to ArrayPrettyPrinter

--- ArrayPrettyPrinter/ArrayPrettyPrinter.h
+++ ArrayPrettyPrinter/ArrayPrettyPrinter.h
@@ -1,11 +1,21 @@
+// Michal Krasowski July 2013
+//
+// ArrayPrettyPrinter.h
+//
 #pragma once
 #include <iostream>
 
+/** 
+ * Prints Array as 2D matrix
+ * \author Michal Krasowski
+ * \date   2013-08-06
+ */
 class ArrayPrettyPrinter
 {
 public:
 	ArrayPrettyPrinter(void);
 	~ArrayPrettyPrinter(void);
+
 
 	template<typename Iterable>
 	static void print(Iterable collection, unsigned int cols)
